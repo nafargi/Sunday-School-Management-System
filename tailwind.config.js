88f2782a84d0aa14b7a2884e3fef95ee65c40788
Nafargi Damena <nafargidamena@gmail.com>
2025-07-06 14:16:48 +0300

    chore:adding tailwind config file and other folders

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // or adapt for Next.js or others
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#9333ea',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
